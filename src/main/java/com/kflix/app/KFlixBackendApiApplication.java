32ca3c63f8846d99f72106e8c24baf8fc6fe2e13
mekontso <mekontsoanderson@gmail.com>
2024-07-08 17:51:09 +0100

    Initial commit

---
+++ src/main/java/com/kflix/app/KFlixBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.kflix.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KFlixBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KFlixBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kflix/app/KFlixBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kflix.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KFlixBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
