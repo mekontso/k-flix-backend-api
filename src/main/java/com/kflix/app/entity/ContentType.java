b44c44e436aeee0bf79c42d134dee37e9f33488d
mekontso <mekontsoanderson@gmail.com>
2024-07-12 14:02:45 +0100

    Add entity

---
+++ src/main/java/com/kflix/app/entity/ContentType.java
@@ -0,0 +1,17 @@
+package com.kflix.app.entity;
+
+import com.kflix.app.util.classes.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class ContentType extends BaseEntity {
+    @Column(unique = true, nullable = false)
+    private String name;
+}
